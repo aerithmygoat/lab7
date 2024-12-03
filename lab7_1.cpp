e96a5ddab51404107c55fcf4d556e148a4a662e2
aerithmygoat <get.bhumiphat@gmail.com>
2024-12-03 14:53:46 +0700

    lab7_1commit

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,10 +1,13 @@
-#include <iostream>
+#include<iostream>
+#include<string>
+using namespace std;
+
 
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
-
-    outpot = a - b/c;
-    count << output: << output << \n;
+    int a  =  20 , b = 30;
+    float c = 4, output;
+    output = (a + b)/c;
+    cout << "output: " << output ;
     return 0;
 }
+
